df1ce29b73805c036569b5de307165541df72b65
AmirsinaRashedi <amirsina.rashedi@gmail.com>
2022-07-03 15:41:04 +0430

    Mean Value with lambda

--- src/StreamExerciseApplication.java
+++ src/StreamExerciseApplication.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.BiConsumer;
 
 import static java.util.stream.Collectors.groupingBy;
 
@@ -11,10 +10,41 @@ public class StreamExerciseApplication {
         List<String> names = Arrays.asList("Amir", "Hatef", "Mehran", "Mojtaba", "Mohammad",
                 "Ali", "Davood", "Reza", "Mohsen");
 
+
         groupByLetters(names);
 
         groupByLettersAndPrintNumberOfNamesInGroup(names);
 
+        meanValueWithLambda();
+
+    }
+
+    private static void meanValueWithLambda() {
+        System.out.println("enter two numbers: ");
+
+        Scanner intInput = new Scanner(System.in);
+
+        Double number1, number2;
+
+        try {
+
+            number1 = intInput.nextDouble();
+
+            number2 = intInput.nextDouble();
+
+        } catch (InputMismatchException e) {
+
+            System.out.println("InputMismatchException");
+
+            return;
+
+        }
+
+        BiConsumer<Double, Double> meanValue = (n1, n2) -> {
+            System.out.println("mean = " + ((n1 + n2) / 2));
+        };
+
+        meanValue.accept(number1, number2);
     }
 
     private static void groupByLettersAndPrintNumberOfNamesInGroup(List<String> names) {
